9703c508034c9a29136aefa9c18009f8bf941659
buran0412 <57691797+buran0412@users.noreply.github.com>
2021-01-05 13:55:46 +0900

    Add files via upload

---
+++ components/SideNavi.vue
@@ -0,0 +1,35 @@
+</template>
+
+<script>
+import SideNavi from "../components/SideNavi";
+import Message from "../components/Message";
+export default {
+  components: {
+    SideNavi,
+    Message
+  }
+};
+</script>
+
+<style scoped>
+.left {
+  width: 22%;
+  height: 100vh;
+}
+.right {
+  width: 78%;
+  height: 100vh;
+}
+.flex {
+  display: flex;
+}
+.title {
+  border-bottom: 1px solid white;
+  border-left: 1px solid white;
+  padding: 15px;
+}
+.title p {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
